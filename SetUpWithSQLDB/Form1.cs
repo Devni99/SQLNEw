86d10b3a74ebd602ced4e3956dcf226aff50d4a7
Devni Abayawardana <68694610+Devni99@users.noreply.github.com>
2021-05-18 18:44:36 +0530

    Add files via upload

---
+++ SetUpWithSQLDB/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SetUpWithSQLDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-CP2CMVQ\SQLEXPRESS;Initial Catalog=SQLDBTest;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Client values (@id,@name,@age)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@name",textBox2.Text);
+            cmd.Parameters.AddWithValue("@age", int.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Successfully Saved");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-CP2CMVQ\SQLEXPRESS;Initial Catalog=SQLDBTest;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Select * from Client", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+    }
+}
---
+++ SetUpWithSQLDB/Form2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SetUpWithSQLDB
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SetUpDB.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Select * from Stud", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = comboBox1.Text;
+            string b = comboBox2.Text;
+            string c = comboBox3.Text;
+
+            string d = a + "." + b + "." + c ;
+            textBox1.Text = d;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SetUpDB.mdf;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert into Stud values (@year,@programme,@Gno,@Gid)", con);
+            cmd.Parameters.AddWithValue("@year", (comboBox1.Text));
+            cmd.Parameters.AddWithValue("@programme", (comboBox2.Text));
+            cmd.Parameters.AddWithValue("@Gno", int.Parse(comboBox3.Text));
+            cmd.Parameters.AddWithValue("@Gid", (textBox1.Text));
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+            MessageBox.Show("Successfully Saved");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SetUpDB.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Select * from Stud", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+    }
+}
